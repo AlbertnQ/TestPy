e41d84043e30070d7dd5c8abcfd411d739e379b4
Sam Fisher <747855841@qq.com>
2024-06-03 16:49:43 +0800

    delete unrelated

--- test.py
+++ test.py
@@ -2,56 +2,6 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        less_than_pivot = [x for x in arr[1:] if x <= pivot]
-        greater_than_pivot = [x for x in arr[1:] if x > pivot]
-        return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
-    
-def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        i = 0
-        j = 0
-        k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
-
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
-
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
-
-
-# 示例用法
-arr = [3, 6, 8, 10, 1, 2, 1]
-sorted_arr = quicksort(arr)
-print(sorted_arr)
-
-merge_sort(arr)
-print(arr)
-
 # 获取股票数据
 symbol = "aapl"
 start_date = "2022-01-01"
