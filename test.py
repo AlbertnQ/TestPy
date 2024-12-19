ebaed4e56318df24dec4f0f305b23f9e4faa29ab
Sam Fisher <747855841@qq.com>
2024-12-19 10:09:53 +0800

    添加工作区配置文件并更新股票数据结束日期

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # 获取股票数据
 symbol = "aapl"
 start_date = "2022-01-01"
-end_date = "2024-06-03"
+end_date = "2024-12-05"
 
 data = yf.download(symbol, start=start_date, end=end_date)
 # 简单的数据分析
